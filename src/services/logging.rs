8caed5e20b2a4cf9996d7191d59fdc0d6391ebf4
AltF2 <contact@altf2.dev>
2022-05-21 15:39:29 +0200

    chore: update rustfmt
    Signed-off-by: AltF2 <contact@altf2.dev>

--- src/services/logging.rs
+++ src/services/logging.rs
@@ -3,9 +3,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn setup() -> Result<()> {
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()),
-        ))
+        .with(tracing_subscriber::EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into())))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
