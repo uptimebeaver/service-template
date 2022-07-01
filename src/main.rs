00fd4712c0332f274f8ceeededf13233845ab50d
AltF2 <contact@altf2.dev>
2022-07-01 18:42:00 +0200

    feature: add service
    Signed-off-by: AltF2 <contact@altf2.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use crate::services::logging;
 use anyhow::Result;
-use tracing::info;
+use tonic::transport::Server;
+use tracing::{info, warn};
 
-mod services;
+use template_lib::services::greeter::GreeterService; // TODO: Change me
 
-fn main() -> Result<()> {
-    logging::setup()?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    utils::logging::setup()?;
 
-    info!("Hello, world!");
+    info!("starting service.");
+
+    let addr = "[::1]:50051".parse()?;
+
+    Server::builder()
+        .add_service(GreeterService::create_server()) // TODO: Change me
+        .serve(addr)
+        .await?;
+
+    warn!("quitting service");
 
     Ok(())
 }
